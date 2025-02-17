8f22d4cad971c7ef46ab786c8a387834732b3f49
Jason Henderson <jasonalanhenderson@gmail.com>
2025-02-16 19:19:02 -0500

    Added in region for customized functions, and renamed variables for clarity.

--- skill-tree/skill_tree.gd
+++ skill-tree/skill_tree.gd
@@ -5,9 +5,16 @@ extends Control
 @onready var tree_root: Button = get_node("Panel/Skill Node")
 
 # Main overload for handling the purchase of nodes
-func handle_node_purchase(node_attributes: String) -> void:
-	match node_attributes:
+func handle_node_purchase(skill_reference: String) -> void:
+	match skill_reference:
 		"FillIn":
-			print_debug("This node was purchased")
-		"Test":
-			print_debug("Another node was puchased")
+			_test_skill_unlocked()
+		_:
+			print_debug("No function assigned with purchased node")
+
+#region Skill Functions
+# INFO Each function in this region is meant to be passed in to handle_node_purchase
+# These functions serve as the logic for each node's activation
+func _test_skill_unlocked() -> void:
+	print_debug("A skill has just been unlocked")
+#endregion Skill Functions
